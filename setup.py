69aa0841c24264d01ef3433ead06e33f06157365
chochohyun <163513440+chochohyun@users.noreply.github.com>
2024-04-22 03:01:06 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
+import setuptools
 
+setuptools.setup (
+  name = 'chocho',
+  version='0.0.1',
+  description='nice calculator',
+  author = 'chohyunju'
+  url = 'https://github.com/chochohyun/chocho.git',
+  download_url = 'https://github.com/chochohyun/chocho.git',
+  packages = ['mycalc'],
+  classifiers =[
+         "Programming Language:: Python :: 3",
+    ]
+
+)
